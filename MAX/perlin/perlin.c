6d2e5ae6294002a0d4959aae135f3a46f24f4a40
GANG IL, LEE <giy.hands@gmail.com>
2022-08-31 09:12:56 +0900

    feat: perlin1D, perlin2D로 정리, CLI/Pointer 추가

--- MAX/perlin/perlin.c
+++ MAX/perlin/perlin.c
@@ -1,169 +1,175 @@
 /**
-	@file
-	perlin
-	giy - giy.hands@gmail.com
+    @file
+    perlin
+    gy - giy.hands@gmail.com 
+
+    @ingroup   noise 
+*/
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include "ext.h"                            // standard Max include, always required
+#include "ext_obex.h"                        // required for new style Max object
+
+/*
+1. 1d noise function - pseudo random: 같은 값이 들어가면 같은 결과
+
+
 */
 
-#include "ext.h"							// standard Max include, always required
-#include "ext_obex.h"						// required for new style Max object
-#include "perlin_lib.h"
 
 ////////////////////////// object struct
-typedef struct _perlin2D
+typedef struct _perlin
 {
-	t_object					ob;			// the object itself (must be first)
+    t_object ob;            // the object itself (must be first)
+    void* outlet_1;
+    long value_prev;
+    long value_current;
     long m_in;
     void* m_proxy;
-	void* m_outlet1;
-	double m_x, m_y;
-	double m_persistence;
-	long m_octave, m_seed;
-} t_perlin2D;
+} t_perlin;
 
 ///////////////////////// function prototypes
 //// standard set
-void *perlin2D_new(t_symbol *s, long argc, t_atom *argv);
-void perlin2D_free(t_perlin2D *x);
-void perlin2D_assist(t_perlin2D *x, void *b, long m, long a, char *s);
-void perlin2D_handle_int(t_perlin2D *x, long i);
-void perlin2D_handle_float(t_perlin2D *x, double f);
-void perlin2D_handle_list(t_perlin2D *x, t_symbol *s, long argc, t_atom *argv);
-
+void *perlin_new(t_symbol *s, long argc, t_atom *argv);
+void perlin_free(t_perlin *x);
+void perlin_assist(t_perlin *x, void *b, long m, long a, char *s);
+void perlin_handle_int(t_perlin* x, long l);
+void perlin_handle_bang(t_perlin* x);
+void perlin_handle_hello(t_perlin* x);
+double perlin_1d_noise(t_perlin* x, double v1);
+    
 //////////////////////// global class pointer variable
-void *perlin2D_class;
+void *perlin_class;
 
 
 void ext_main(void *r)
 {
-	t_class *c;
+    t_class *c;
 
-	c = class_new("perlin2D", (method)perlin2D_new, (method)perlin2D_free, (long)sizeof(t_perlin2D),
-				  0L /* leave NULL!! */, A_GIMME, 0);
+    c = class_new("perlin", (method)perlin_new, (method)perlin_free, (long)sizeof(t_perlin),
+                  0L /* leave NULL!! */, A_GIMME, 0);
 
-	/* you CAN'T call this from the patcher */
-	class_addmethod(c, (method)perlin2D_assist,			"assist",		A_CANT, 0);
-	class_addmethod(c, (method)perlin2D_handle_int,		"int",			A_LONG, 0);
-	class_addmethod(c, (method)perlin2D_handle_float,	"float",		A_FLOAT, 0);
-	class_addmethod(c, (method)perlin2D_handle_list,	"list",			A_GIMME, 0);
+    /* you CAN'T call this from the patcher */
+    class_addmethod(c, (method)perlin_assist, "assist", A_CANT, 0);
+    class_addmethod(c, (method)perlin_handle_int, "int", A_LONG, 0);
+    class_addmethod(c, (method)perlin_handle_bang, "bang", 0);
+    class_addmethod(c, (method)perlin_handle_hello, "hello", 0);
 
-	class_register(CLASS_BOX, c); /* CLASS_NOBOX */
-	perlin2D_class = c;
+    class_register(CLASS_BOX, c); /* CLASS_NOBOX */
+    perlin_class = c;
 
-	// post("I am the perlin2D object");
+    post("I am the perlin object");
 }
 
-void perlin2D_assist(t_perlin2D *x, void *b, long m, long a, char *s)
+void perlin_handle_hello(t_perlin* x) {
+    object_post((t_object*)x, "Hello!!!");
+}
+
+void perlin_handle_bang(t_perlin* x) {
+    
+    long n = proxy_getinlet((t_object*)x); // inlet의 번호
+//    outlet_bang(x->outlet_1);
+    
+    if (n == 0) { // 왼쪽 inlet으로 bang이 들어올 때,
+        
+        long result = x->value_prev + x->value_current;
+        
+        if (result > 4660046610375530309) {
+            x->value_prev = 0; // reset
+            x->value_current = 1;
+            object_post((t_object*)x, "Reset!");
+            result = x->value_prev + x->value_current;
+        }
+        outlet_int(x->outlet_1, result);
+        
+        x->value_prev = x->value_current;
+        x->value_current = result;
+        
+    } else if (n == 1) { // 오른쪽 inlet으로 bang이 들어올 때,
+        x->value_prev = 0; // reset
+        x->value_current = 1;
+    } else {
+        post("from inlet %d", n);
+    }
+    
+}
+
+// http://libnoise.sourceforge.net/noisegen/index.html#linearcoherentnoise
+double IntegerNoise (int n)
 {
-	if (m == ASSIST_INLET) { // inlet
-		sprintf(s, "I am inlet %ld", a);
-	}
-	else {	// outlet
-		sprintf(s, "I am outlet %ld", a);
-	}
+    n = (n >> 13) ^ n;
+    int nn = (n * (n * n * 60493 + 19990303) + 1376312589) & 0x7fffffff;
+    return 1.0 - ((double)nn / 1073741824.0); // -1.0 ~ 1.0
 }
 
-void perlin2D_handle_int(t_perlin2D *x, long i) {
-
-	double rslt;
-	switch(proxy_getinlet((t_object*)x)) {
-		case 2:
-			// object_post((t_object*)x, "float received in inlet 0");
-			x->m_octave = i;
-			break;
-
-		case 3:
-			// object_post((t_object*)x, "float received in inlet 1");
-			x->m_seed = i;
-			break;
-
-		default:
-			break;
-	}
+void perlin_handle_int(t_perlin* x, long ld) {
+    // object_post((t_object*)x, "I got %ld", ld);
+    long r = IntegerNoise(ld);
+    outlet_int(x->outlet_1, r);
 }
 
-void perlin2D_handle_float(t_perlin2D *x, double f) {
-
-	double rslt;
-	switch(proxy_getinlet((t_object*)x)) {
-		case 1:
-			x->m_persistence = f;
-			break;
-
-		default:
-			break;
-	}
-}
-
-void perlin2D_handle_list(t_perlin2D *x, t_symbol *s, long argc, t_atom *argv) {
-	long i;
-    t_atom *ap;
-	double val1, val2, rslt;
- 
-    // post("message selector is %s",s->s_name);
-    // post("there are %ld arguments",argc);
-
-	if (argc == 1) {
-		val1 = atom_getfloat(argv);
-		rslt = pnoise1d(val1, x->m_persistence, x->m_octave, x->m_seed);
-	} else if (argc == 2) {
-		val1 = atom_getfloat(argv + 0);
-		val2 = atom_getfloat(argv + 1);
-		rslt = pnoise2d(val1, val2, x->m_persistence, x->m_octave, x->m_seed);
-	}
-	outlet_float(x->m_outlet1, rslt);
-}
-
-void perlin2D_free(t_perlin2D *x)
+void perlin_assist(t_perlin *x, void *b, long m, long a, char *s)
 {
-	;
+    if (m == ASSIST_INLET) { // inlet
+        sprintf(s, "I am inlet %ld", a);
+    }
+    else {    // outlet
+        sprintf(s, "I am outlet %ld", a);
+    }
 }
 
-void *perlin2D_new(t_symbol *s, long argc, t_atom *argv)
+void perlin_free(t_perlin *x)
 {
-	t_perlin2D *x = NULL;
-	long i;
-
-	if ((x = (t_perlin2D *)object_alloc(perlin2D_class))) {
-		// object_post((t_object *)x, "a new %s object was instantiated: %p", s->s_name, x);
-		// object_post((t_object *)x, "it has %ld arguments", argc);
-
-		// inlets
-		x->m_proxy = proxy_new((t_object*)x, 3, &x->m_in);
-		x->m_proxy = proxy_new((t_object*)x, 2, &x->m_in);
-		x->m_proxy = proxy_new((t_object*)x, 1, &x->m_in);
-
-		// outlets
-		x->m_outlet1 = outlet_new((t_object*)x, NULL);
-
-		// init values
-		x->m_persistence = 0.7;
-		x->m_octave = 5;
-		x->m_seed = 12124;
-
-		for (i = 0; i < argc; i++) {
-			if ((argv + i)->a_type == A_LONG) {
-				long v = atom_getlong(argv+i);
-				// object_post((t_object *)x, "arg %ld: long (%ld)", i, v);
-				if (i == 1){
-					x->m_octave = v;
-					object_post((t_object *)x, "octave is set : %ld", x->m_octave);
-				} else if (i == 2) {
-					x->m_seed = v;
-					object_post((t_object *)x, "seed is set : %ld", x->m_seed);
-				} 
-			} else if ((argv + i)->a_type == A_FLOAT) {
-				double v = atom_getfloat(argv+i);
-				// object_post((t_object *)x, "arg %ld: float (%f)", i, v);
-				if (i == 0){
-					x->m_persistence = v;
-					object_post((t_object *)x, "persistence is set : %f", x->m_persistence);
-				}
-			} else if ((argv + i)->a_type == A_SYM) {
-				object_post((t_object *)x, "arg %ld: symbol (%s)", i, atom_getsym(argv+i)->s_name);
-			} else {
-				object_error((t_object *)x, "forbidden argument");
-			}
-		}
-	}
-	return (x);
+    ;
 }
+
+
+
+double perlin_1d_noise(t_perlin* x, double v1) {
+
+    long * iptr;
+    double f;
+    f = modf(sin(v1) * 43758.5453123, &iptr);
+
+    print("f: %f\n", f)
+
+    return f;
+}
+
+
+void *perlin_new(t_symbol *s, long argc, t_atom *argv)
+{
+    t_perlin *x = NULL;
+    long i;
+
+    if ((x = (t_perlin *)object_alloc(perlin_class))) {
+        object_post((t_object *)x, "a new %s object was instantiated: %p", s->s_name, x);
+        object_post((t_object *)x, "it has %ld arguments", argc);
+        
+        // inlet 만들기
+        x->m_proxy = proxy_new((t_object*)x, 1, &x->m_in);
+        
+        // outlet 만들기
+        x->outlet_1 = outlet_new(x, NULL); // NULL을 사용하면 어떤 타입이든 내보낼 수 있다
+        
+        // 변수 초기화
+        x->value_prev = 0;
+        x->value_current = 1;
+
+        for (i = 0; i < argc; i++) {
+            if ((argv + i)->a_type == A_LONG) {
+                object_post((t_object *)x, "arg %ld: long (%ld)", i, atom_getlong(argv+i));
+            } else if ((argv + i)->a_type == A_FLOAT) {
+                object_post((t_object *)x, "arg %ld: float (%f)", i, atom_getfloat(argv+i));
+            } else if ((argv + i)->a_type == A_SYM) {
+                object_post((t_object *)x, "arg %ld: symbol (%s)", i, atom_getsym(argv+i)->s_name);
+            } else {
+                object_error((t_object *)x, "forbidden argument");
+            }
+        }
+    }
+    return (x);
+}
+
+//argv+0 ===> &argv[0]
+//argv+1 ===> &argv[1]
